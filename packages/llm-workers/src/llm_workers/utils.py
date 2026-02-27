533eb143f75bb6a34af3ed3ce38d2a50a7f3e936
Dmitry Mikhaylov <dmitry@smallgiantgames.com>
2026-02-27 13:09:51 +0200

    minor warning fix

--- packages/llm-workers/src/llm_workers/utils.py
+++ packages/llm-workers/src/llm_workers/utils.py
@@ -61,7 +61,7 @@ def get_env_var_or_fail(name: str) -> str:
         raise OSError(f"Environment variable {name} not set")
     return var
 
-def ensure_environment_variable(environment: Dict[str,str], var_name: str, description: any = None, is_persistent: bool = True, is_secret: bool = False) -> str:
+def ensure_environment_variable(environment: Dict[str,str], var_name: str, description: Any = None, is_persistent: bool = True, is_secret: bool = False) -> str:
     """
     Ensure an environment variable is set, prompting the user if it's missing.
 
